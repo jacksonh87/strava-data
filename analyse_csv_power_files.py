482faca2a3cbb11078209f52f9d8d34ba243ebba
Jackson <jackson.d.hunter@gmail.com>
2019-07-30 22:51:40 +0100

    More elegant way of specifying durations and storing results

--- analyse_csv_power_files.py
+++ analyse_csv_power_files.py
@@ -5,62 +5,47 @@ all-time top power for ten seconds, one minure,
 five minutes, ten minutes, twenty minutes, forty 
 minutes and sixty minutes
 """
+import json
 import os
+import matplotlib.pyplot as plt
 import pandas as pd
-import webbrowser
+
+
 
 def analyse_csv_power_files():
     source_files_directory = './power_data/'
     
     results_dict = {}
+    power_durations = [3, 4, 5, 10, 15, 30, 45, 60, 90, 120, 180, 240, 5*60, 6*60, 7*60, 8*60, 9*60, 10*60, 15*60, 20*60, 30*60, 40*60, 60*60, 90*60, 120*60]
+
     for file in os.listdir(source_files_directory):
         csv_file_path = source_files_directory+file
         activity_id = file.strip('.csv')
         df = pd.read_csv(csv_file_path)
         if 'watts' in df.columns:
-            df['ten_sec_power'] = df.watts.rolling(10).mean().fillna(0.00)
-            df['one_min_power'] = df.watts.rolling(60).mean().fillna(0.00)
-            df['five_min_power'] = df.watts.rolling(5*60).mean().fillna(0.00)
-            df['ten_min_power'] = df.watts.rolling(10*60).mean().fillna(0.00)
-            df['twenty_min_power'] = df.watts.rolling(20*60).mean().fillna(0.00)
-            df['forty_min_power'] = df.watts.rolling(40*60).mean().fillna(0.00)
-            df['sixty_min_power'] = df.watts.rolling(60*60).mean().fillna(0.00)
-            results_dict[activity_id] = [max(df.ten_sec_power), max(df.one_min_power), max(df.five_min_power), max(df.ten_min_power), max(df.twenty_min_power), max(df.forty_min_power), max(df.sixty_min_power)]
+            results = []
+            for duration in power_durations:
+                df[duration] = df.watts.rolling(duration).mean().fillna(0.00)
+                results.append(max(df[duration]))                
+            results_dict[activity_id] = results
     
     best_efforts_df = pd.DataFrame(results_dict).T
-    best_efforts_df.columns = ['ten_sec_power', 
-                               'one_min_power', 
-                               'five_min_power', 
-                               'ten_min_power',
-                               'twenty_min_power',
-                               'forty_min_power',
-                               'sixty_min_power']
-    
+    best_efforts_df.columns = power_durations  
     best_efforts_df.to_csv('best_power_efforts.csv')
     
-    best_ten_sec = best_efforts_df[best_efforts_df.ten_sec_power==best_efforts_df.ten_sec_power.max()]
-    best_one_min = best_efforts_df[best_efforts_df.one_min_power==best_efforts_df.one_min_power.max()]
-    best_five_min = best_efforts_df[best_efforts_df.five_min_power==best_efforts_df.five_min_power.max()]
-    best_ten_min = best_efforts_df[best_efforts_df.ten_min_power==best_efforts_df.ten_min_power.max()]
-    best_twenty_min = best_efforts_df[best_efforts_df.twenty_min_power==best_efforts_df.twenty_min_power.max()]
-    best_forty_min = best_efforts_df[best_efforts_df.forty_min_power==best_efforts_df.forty_min_power.max()]
-    best_sixty_min = best_efforts_df[best_efforts_df.sixty_min_power==best_efforts_df.sixty_min_power.max()]
+    best_efforts_dict = {}
+    best_efforts_ids_dict = {}
     
-    print('Best 10 second power: '+ str(round(best_efforts_df.ten_sec_power.max()))+ ' in activity: '+str(best_ten_sec.index[0]))
-    print('Best 1 minute power: '+ str(round(best_efforts_df.one_min_power.max()))+ ' in activity: '+str(best_one_min.index[0]))
-    print('Best 5 minute power: '+ str(round(best_efforts_df.five_min_power.max()))+ ' in activity: '+str(best_five_min.index[0]))
-    print('Best 10 minute power: '+ str(round(best_efforts_df.ten_min_power.max()))+ ' in activity: '+str(best_ten_min.index[0]))
-    print('Best 20 minute power: '+ str(round(best_efforts_df.twenty_min_power.max()))+ ' in activity: '+str(best_twenty_min.index[0]))
-    print('Best 40 minute power: '+ str(round(best_efforts_df.forty_min_power.max()))+ ' in activity: '+str(best_forty_min.index[0]))
-    print('Best 60 minute power: '+ str(round(best_efforts_df.sixty_min_power.max()))+ ' in activity: '+str(best_sixty_min.index[0]))
-    
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_ten_sec.index[0]))
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_one_min.index[0]))
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_five_min.index[0]))
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_ten_min.index[0]))
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_twenty_min.index[0]))
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_forty_min.index[0]))
-    #webbrowser.open_new_tab('https://www.strava.com/activities/'+str(best_sixty_min.index[0]))
-    
+    for duration in power_durations:
+        best_effort = best_efforts_df[best_efforts_df[duration]==best_efforts_df[duration].max()]
+        best_efforts_dict[duration] = best_effort[duration][0]
+        best_efforts_ids_dict[duration] = best_effort.index[0]
+        
+    best_efforts_json_string = json.dumps(best_efforts_dict)
+    with open('best_efforts.json', 'w+') as f:
+        json.dump(best_efforts_json_string, f)
+            
+    return best_efforts_dict, best_efforts_ids_dict, best_efforts_df
+        
 if __name__ == '__main__':
-    analyse_csv_power_files()
+    best_efforts_dict, best_efforts_ids_dict, best_efforts_df = analyse_csv_power_files()
